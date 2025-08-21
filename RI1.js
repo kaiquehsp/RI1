a0a28c58ea22b084ae66698da2ea3ae50e6147e9
kaiquehsp <kaiquehsp5@gmail.com>
2025-08-20 21:32:54 -0300

    Add files via upload

---
+++ RI1.js
@@ -0,0 +1,356 @@
+const panteraNegra = {
+    nome: "T'Challa",
+    codinome: "Pantera Negra",
+    armaPrincipal: "Garras de Vibranium",
+    armaSecundaria: "Escudo de Vibranium",
+    velocidade: 90,
+    forca: 80,
+    resistencia: 85,
+    descricao: function () {
+      return (
+        "Nome do personagem: " +
+        this.nome +
+        "\n" +
+        "Codinome do personagem: " +
+        this.codinome +
+        "\n" +
+        "Arma principal: " +
+        this.armaPrincipal +
+        "\n" +
+        "Arma secundaria: " +
+        this.armaSecundaria +
+        "\n" +
+        "Nível de força: " +
+        this.forca +
+        "\n" +
+        "Nível de velocidade: " +
+        this.velocidade +
+        "\n" +
+        "Nível de resistência: " +
+        this.resistencia
+      );
+    },
+  };
+  
+  const homemAranha = {
+    nome: "Peter Parker",
+    codinome: "Homem-Aranha",
+    armaPrincipal: "Teias",
+    armaSecundaria: "Sentido Aranha",
+    velocidade: 95,
+    forca: 70,
+    resistencia: 80,
+    descricao: function () {
+      return (
+        "Nome do personagem: " +
+        this.nome +
+        "\n" +
+        "Codinome do personagem: " +
+        this.codinome +
+        "\n" +
+        "Arma principal: " +
+        this.armaPrincipal +
+        "\n" +
+        "Arma secundaria: " +
+        this.armaSecundaria +
+        "\n" +
+        "Nível de força: " +
+        this.forca +
+        "\n" +
+        "Nível de velocidade: " +
+        this.velocidade +
+        "\n" +
+        "Nível de resistência: " +
+        this.resistencia
+      );
+    },
+  };
+  
+  const doutorEstranho = {
+    nome: "Stephen Strange",
+    codinome: "Doutor Estranho",
+    armaPrincipal: "Olho de Agamotto",
+    armaSecundaria: "Feitiçaria",
+    velocidade: 60,
+    forca: 60,
+    resistencia: 75,
+    descricao: function () {
+      return (
+        "Nome do personagem: " +
+        this.nome +
+        "\n" +
+        "Codinome do personagem: " +
+        this.codinome +
+        "\n" +
+        "Arma principal: " +
+        this.armaPrincipal +
+        "\n" +
+        "Arma secundaria: " +
+        this.armaSecundaria +
+        "\n" +
+        "Nível de força: " +
+        this.forca +
+        "\n" +
+        "Nível de velocidade: " +
+        this.velocidade +
+        "\n" +
+        "Nível de resistência: " +
+        this.resistencia
+      );
+    },
+  };
+  
+  const vingadoraVermelha = {
+    nome: "Carol Danvers",
+    codinome: "Vingadora Vermelha",
+    armaPrincipal: "Poder Cósmico",
+    armaSecundaria: "Raio de Energia",
+    velocidade: 85,
+    forca: 95,
+    resistencia: 90,
+    descricao: function () {
+      return (
+        "Nome do personagem: " +
+        this.nome +
+        "\n" +
+        "Codinome do personagem: " +
+        this.codinome +
+        "\n" +
+        "Arma principal: " +
+        this.armaPrincipal +
+        "\n" +
+        "Arma secundaria: " +
+        this.armaSecundaria +
+        "\n" +
+        "Nível de força: " +
+        this.forca +
+        "\n" +
+        "Nível de velocidade: " +
+        this.velocidade +
+        "\n" +
+        "Nível de resistência: " +
+        this.resistencia
+      );
+    },
+  };
+  
+  const visão = {
+    nome: "Vision",
+    codinome: "Visão",
+    armaPrincipal: "Raio de Massa",
+    armaSecundaria: "Força de Denso",
+    velocidade: 75,
+    forca: 80,
+    resistencia: 90,
+    descricao: function () {
+      return (
+        "Nome do personagem: " +
+        this.nome +
+        "\n" +
+        "Codinome do personagem: " +
+        this.codinome +
+        "\n" +
+        "Arma principal: " +
+        this.armaPrincipal +
+        "\n" +
+        "Arma secundaria: " +
+        this.armaSecundaria +
+        "\n" +
+        "Nível de força: " +
+        this.forca +
+        "\n" +
+        "Nível de velocidade: " +
+        this.velocidade +
+        "\n" +
+        "Nível de resistência: " +
+        this.resistencia
+      );
+    },
+  };
+  
+  const mortoVivo = {
+    nome: "Norman Osborn",
+    codinome: "Duende Verde",
+    armaPrincipal: "Pumpkin Bomb",
+    armaSecundaria: "Planos Psicológicos",
+    velocidade: 70,
+    forca: 85,
+    resistencia: 80,
+    descricao: function () {
+      return (
+        "Nome do personagem: " +
+        this.nome +
+        "\n" +
+        "Codinome do personagem: " +
+        this.codinome +
+        "\n" +
+        "Arma principal: " +
+        this.armaPrincipal +
+        "\n" +
+        "Arma secundaria: " +
+        this.armaSecundaria +
+        "\n" +
+        "Nível de força: " +
+        this.forca +
+        "\n" +
+        "Nível de velocidade: " +
+        this.velocidade +
+        "\n" +
+        "Nível de resistência: " +
+        this.resistencia
+      );
+    },
+  };
+  
+  const panteraGuerreira = {
+    nome: "Shuri",
+    codinome: "Pantera Guerreira",
+    armaPrincipal: "Armadura de Vibranium",
+    armaSecundaria: "Dispositivos Tecnológicos",
+    velocidade: 90,
+    forca: 75,
+    resistencia: 80,
+    descricao: function () {
+      return (
+        "Nome do personagem: " +
+        this.nome +
+        "\n" +
+        "Codinome do personagem: " +
+        this.codinome +
+        "\n" +
+        "Arma principal: " +
+        this.armaPrincipal +
+        "\n" +
+        "Arma secundaria: " +
+        this.armaSecundaria +
+        "\n" +
+        "Nível de força: " +
+        this.forca +
+        "\n" +
+        "Nível de velocidade: " +
+        this.velocidade +
+        "\n" +
+        "Nível de resistência: " +
+        this.resistencia
+      );
+    },
+  };
+  
+  const Thanos = {
+    nome: "Josh Brolin",
+    codinome: "Thanos",
+    armaPrincipal: "Manopla do inifinito",
+    armaSecundaria: "",
+    velocidade: 90,
+    forca: 100,
+    resistencia: 100,
+    descricao: function () {
+      return (
+        "Nome do personagem: " +
+        this.nome +
+        "\n" +
+        "Codinome do personagem: " +
+        this.codinome +
+        "\n" +
+        "Arma principal: " +
+        this.armaPrincipal +
+        "\n" +
+        "Arma secundaria: " +
+        this.armaSecundaria +
+        "\n" +
+        "Nível de força: " +
+        this.forca +
+        "\n" +
+        "Nível de velocidade: " +
+        this.velocidade +
+        "\n" +
+        "Nível de resistência: " +
+        this.resistencia
+      );
+    },
+  };
+  
+  const personagens = [
+    panteraNegra,
+    homemAranha,
+    doutorEstranho,
+    vingadoraVermelha,
+    visão,
+    mortoVivo,
+    panteraGuerreira,
+    Thanos,
+  ];
+  
+  function compararPersonagens() {
+    let vitoriaForca = "";
+    let vitoriaVelocidade = "";
+    let vitoriaResistencia = "";
+    let vitoriaOverall = "";
+  
+    for (let i = 0; i < personagens.length; i++) {
+      for (let j = i + 1; j < personagens.length; j++) {
+        const p1 = personagens[i];
+        const p2 = personagens[j];
+  
+        const p1ForcaTotal =
+          p1.forca +
+          (p1.armaPrincipal === "Escudo de Vibranium"
+            ? 15
+            : p1.armaPrincipal === "Armadura de Vibranium"
+            ? 20
+            : 0);
+        const p2ForcaTotal =
+          p2.forca +
+          (p2.armaPrincipal === "Escudo de Vibranium"
+            ? 15
+            : p2.armaPrincipal === "Armadura de Vibranium"
+            ? 20
+            : 0);
+  
+        if (p1ForcaTotal > p2ForcaTotal) {
+          vitoriaForca = `${p1.codinome} vence em força contra ${p2.codinome}.`;
+        } else if (p1ForcaTotal < p2ForcaTotal) {
+          vitoriaForca = `${p2.codinome} vence em força contra ${p1.codinome}.`;
+        } else {
+          vitoriaForca = `${p1.codinome} e ${p2.codinome} têm a mesma força.`;
+        }
+  
+        if (p1.velocidade > p2.velocidade) {
+          vitoriaVelocidade = `${p1.codinome} vence em velocidade contra ${p2.codinome}.`;
+        } else if (p1.velocidade < p2.velocidade) {
+          vitoriaVelocidade = `${p2.codinome} vence em velocidade contra ${p1.codinome}.`;
+        } else {
+          vitoriaVelocidade = `${p1.codinome} e ${p2.codinome} têm a mesma velocidade.`;
+        }
+  
+        if (p1.resistencia > p2.resistencia) {
+          vitoriaResistencia = `${p1.codinome} vence em resistência contra ${p2.codinome}.`;
+        } else if (p1.resistencia < p2.resistencia) {
+          vitoriaResistencia = `${p2.codinome} vence em resistência contra ${p1.codinome}.`;
+        } else {
+          vitoriaResistencia = `${p1.codinome} e ${p2.codinome} têm a mesma resistência.`;
+        }
+  
+        const p1Overall = p1ForcaTotal + p1.velocidade + p1.resistencia;
+        const p2Overall = p2ForcaTotal + p2.velocidade + p2.resistencia;
+  
+        if (p1Overall > p2Overall) {
+          vitoriaOverall = `${p1.codinome} vence no geral contra ${p2.codinome}.`;
+        } else if (p1Overall < p2Overall) {
+          vitoriaOverall = `${p2.codinome} vence no geral contra ${p1.codinome}.`;
+        } else {
+          vitoriaOverall = `${p1.codinome} e ${p2.codinome} têm o mesmo desempenho no geral.`;
+        }
+  
+        console.log(p1.descricao());
+        console.log(p2.descricao());
+        console.log(vitoriaForca);
+        console.log(vitoriaVelocidade);
+        console.log(vitoriaResistencia);
+        console.log(vitoriaOverall);
+        console.log("---------------------------------");
+      }
+    }
+  }
+  
+  compararPersonagens();
